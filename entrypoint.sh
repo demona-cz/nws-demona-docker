12445d151fc29e63e5149da24cde7cc218b6a522
Ter Leeloo II <47660244+ter-leeloo-ii@users.noreply.github.com>
2020-11-03 10:27:38 +0100

    initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+
+startTimestamp=$(date +%Y%m%d%H%M%S)
+
+# cleanup unnecessary files
+rm -f .nwnpid
+rm -rf currentgame.*
+rm -rf temp.*
+
+# handle auto-rotating logs
+if [ $NWS_ROTATE_LOGS = true -a -w $NWS_LOGS_DIR ]; then
+
+    logsDir=$NWS_LOGS_DIR/logs-$startTimestamp
+    mkdir $logsDir
+    ln -sfn $logsDir logs.0
+fi
+
+# handle auto-rotating (versioning) modules
+# mark new modules with start timestamp
+moduleToStart=$NWS_MODULE
+if [ $NWS_ROTATE_MODULES = true ]; then
+
+    # mark all modules with timestamp prefix
+    for f in $(find modules -maxdepth 1 -regex "^modules\/$NWS_MODULE-[0-9]*\.[0-9]*\.[0-9]*\.mod"); do
+        mv $f modules/$startTimestamp-$(basename $f)
+    done
+
+    # resolve newest version
+    moduleToStart=$(find modules -maxdepth 1 -regex "^modules\/[0-9]*-$NWS_MODULE-[0-9]*\.[0-9]*\.[0-9]*\.mod" | sort -r | head -1 | xargs -i basename {} .mod)
+    if [ -e $moduleToStart ]; then
+        echo 'no module to start'
+        exit 1
+    fi
+fi
+
+exec dockerize -template ./nwnplayer.ini.tmpl:./nwnplayer.ini ./nwserver -module $moduleToStart
